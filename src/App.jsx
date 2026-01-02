ed2f82067bfdfd5759c1a93a79553f937f210eee
Aditya Lad <adityanlad@gmail.com>
2026-01-02 23:41:48 +0530

    cha

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,16 @@ function App() {
         <div className="container  mx-auto my-5 px-5 rounded-xl p-4 bg-violet-100 min-h-[80vh]">
           <div className="addTodos">
             <h2 className="text-lg font-bold">Add a Todo</h2>
-            <input type="text" className="border rounded px-2 py-1 mr-2"/>
-            <button className="bg-purple-500 text-white px-3 py-1 rounded">Add</button>
+            <input type="text" className="border rounded px-2 py-1 mr-2 w-1/2"/>
+            <button className="bg-purple-500  hover:bg-purple-800 text-sm font-bold text-white px-3 py-1 rounded">Add</button>
           </div>
           <h2 className="text-xl font-bold">Your Todos</h2>
         <div className="todos">
           <div className="todo flex justify-between">
             <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
-            <div className="buttons">
-              <button>Edit</button>
-              <button>Delete</button>
+            <div className="buttons flex gap-3">
+              <button  className="bg-purple-500 hover:bg-purple-800 text-sm font-bold text-white px-2 py-1 rounded">Edit</button>
+              <button  className="bg-purple-500 hover:bg-purple-800 text-sm font-bold text-white px-2 py-1 rounded">Delete</button>
             </div>
             
           </div>
